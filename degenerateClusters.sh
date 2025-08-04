6fa038283c5535b3782e0dca5d71e29d77016492
Kris Booker <kristopher.booker@tomorrow.io>
2025-08-04 09:05:53 -0600

    Updating with proper config and new image.

--- degenerateClusters.sh
+++
@@ -1,5 +0,0 @@
-for i in $(seq 1 1 60)
-    do 
-        #pcluster create-cluster --region us-east-1 --cluster-name da-training-cluster-$i --cluster-configuration da_hpc.yaml --rollback-on-failure false --debug
-        pcluster delete-cluster --region us-east-1 --cluster-name da-training-cluster-$i 
-    done
