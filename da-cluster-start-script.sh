84d4dae95a5bbb50e784c53153fab07ccbb49296
Kris Booker <kristopher.booker@tomorrow.io>
2025-08-25 10:11:45 -0600

    Last fix for grads.

--- da-cluster-start-script.sh
+++ da-cluster-start-script.sh
@@ -369,7 +369,32 @@ spack install grads 2>&1 | tee log.install_grads.001
 cd /opt/modulefiles
 mkdir grads
 cd grads
-curl -O https://raw.githubusercontent.com/ulmononian/ihaul/refs/heads/main/2.2.3.lua
+
+export GRADSPATH=`ls -d /opt/spack/opt/spack/linux-sapphirerapids/grads-2.2.3-*`
+
+tee /opt/modulefiles/grads/2.2.3.lua <<EOF
+
+whatis("Name : grads")
+whatis("Version : 2.2.1")
+whatis("Target : x86_64_v4")
+whatis("Short description : The Grid Analysis and Display System (GrADS) is an interactive desktop tool that is used for easy access, manipulation, and visualization of earth science data. GrADS has two data models for handling gridded and station data. GrADS supports many data file formats, including binary (stream or sequential), GRIB (version 1 and 2), NetCDF, HDF (version 4 and 5), and BUFR (for station data).")
+whatis("Configure options : --with-geotiff --with-hdf4 --with-hdf5 --with-netcdf --without-gadap")
+help([[Name   : grads]])
+help([[Version: 2.2.3]])
+help([[Target : x86_64_v4]])
+help()
+help([[The Grid Analysis and Display System (GrADS) is an interactive desktop
+tool that is used for easy access, manipulation, and visualization of
+earth science data. GrADS has two data models for handling gridded and
+station data. GrADS supports many data file formats, including binary
+(stream or sequential), GRIB (version 1 and 2), NetCDF, HDF (version 4
+and 5), and BUFR (for station data).]])
+prepend_path("PATH","${GRADSPATH}/bin")
+prepend_path("LD_LIBRARY_PATH","${GRADSPATH}/lib")
+setenv("GADDIR","${GRADSPATH}/data")
+setenv("GRADS_ROOT","${GRADSPATH}")
+
+EOF
 
 ## Add ImgCAT
 cd /opt
